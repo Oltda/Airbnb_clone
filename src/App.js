d6321ae40b45885fc39612d01fe3aacc4851b1d8
Daniel Olt <dan.olt@hotmail.cz>
2021-09-05 16:25:38 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import NavBar from './utility/NavBar/NavBar';
+import Home from './pages/Home/Home'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import SingleFullVenue from './pages/SingleFullVenue/SingleFullVenue';
+import Modal from './utility/Modal/Modal';
+import CityVenues from './pages/CityVenues/CityVenues';
+import PaymentSuccess from './pages/PaymentSuccess/PaymentSuccess';
+import Account from './pages/Account/Account';
+import Search from './pages/Search/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+          <Route path="/" component={NavBar} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/venue/:vid" component={SingleFullVenue} />
+          <Route exact path="/city/:cityName" component={CityVenues} />
+          <Route exact path="/payment-success/:stripeToken" component={PaymentSuccess} />
+          <Route path="/" component={Modal} />
+          <Route path="/account" component={Account} />
+          <Route path="/search/:searchTerm" component={Search} />
+
+      </Router>
+    );
+  }
+
 }
 
 export default App;
